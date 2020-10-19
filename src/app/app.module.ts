6b4d4b3b1ae226fa370d02f095267d78be742f46
pratisingh <pratsing26@gmail.com>
2020-10-18 23:15:25 -0400

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
-import { FormsModule } from '@angular/forms';
-import { ChartsModule } from 'ng2-charts';
-import { MortageSummaryComponent } from './components/mortage-summary/mortage-summary.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
+import { ChartsModule } from "ng2-charts";
+import { MortageSummaryComponent } from "./components/mortage-summary/mortage-summary.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MortageSummaryComponent
-    
-  ],
-  imports: [
-    FormsModule,
-    BrowserModule,
-    ChartsModule,
-    BrowserAnimationsModule,
-    NgxChartsModule
-  ],
+  declarations: [AppComponent, MortageSummaryComponent],
+  imports: [FormsModule, BrowserModule, ChartsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
